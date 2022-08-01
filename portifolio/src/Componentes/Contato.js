2ef88b946540a1492c4c08a122f45a89829e783a
luizmensato <luizmensato@hotmail.com>
2022-08-01 11:54:13 -0300

    Adicionei o componente Contato.js e fiz o roteamento no index.js

---
+++ portifolio/src/Componentes/Contato.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header'
+import Contact from './Main/Contact';
+import Footer from './Footer';
+
+
+
+export default class Contato extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Contact/>
+        <Footer/>
+      </>
+
+
+
+    );
+  }
+
+}
+
--- portifolio/src/Componentes/Header/Nav.js
+++ portifolio/src/Componentes/Header/Nav.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { Link } from 'react-router-dom';
 
 export default class Nav extends React.Component {
   render() {
     return (
       <section className="nav">
-        <AnchorLink href='#home' offset={80}>Home</AnchorLink>
-        <AnchorLink href='#photo' offset={80}>Fotos</AnchorLink>
-        <AnchorLink href='#information'>Informações</AnchorLink>
-        <AnchorLink href='#contact'>Contato</AnchorLink>
+        <Link to="/">Home</Link>
+        <Link to="/contato">Contato</Link>
+        
       </section>
     );
   }
